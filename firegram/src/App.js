99175f88a6cdc7e8581e276a1fc6f3ff009fd945
Fardeen Khan <khanfardeen9983@gmail.com>
2020-07-30 00:54:49 +0530

    Firegram - Project setup

---
+++ firegram/src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import logo from "./logo.svg";
+import Title from "./comps/Title";
+
+function App() {
+  return <div className="App">
+    <Title />
+  </div>;
+}
+
+export default App;
