5654191f925529df317f90acdeea4e8bf7fd7f18
Fardeen Khan <khanfardeen9983@gmail.com>
2020-08-01 23:21:05 +0530

    Socket-Chat : Start

---
+++ Components/src/comp/ToggleSwitch.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./ToggleSwitch.css";
+
+export default function ToggleSwitch() {
+  return (
+    <label className="toggle-switch">
+      <input type="checkbox" />
+          <span class="switch" />
+    </label>
+  );
+}
---
+++ Projects/Socket-Chat/Readme.md
@@ -0,0 +1,4 @@
+# Chat App
+### Using React and Socket.io
+
+### Reference [Youtube Video](https://www.youtube.com/watch?v=ZwFA3YMfkoc&list=PLMboSA8wsgUGtd59qmi8NYDRQThnvggI6)
