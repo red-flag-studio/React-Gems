c0dbf46c988fd05f9f0eac2ec7c5d215cbd18a61
Fardeen Khan <khanfardeen9983@gmail.com>
2020-08-02 13:46:09 +0530

    Socket-Chat : Socket server setup

---
+++ Projects/Socket-Chat/server/app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const socketio = require("socket.io");
+const http = require("http");
+
+const router = require("./router");
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+io.on('connection',(socket)=>{
+    console.log("New connection");
+    socket.on("disconnect",()=>{
+        console.log("User Disconnected");
+    });
+})
+app.use(router);
+ 
+server.listen(PORT, () => console.log(`Server started on ${PORT}`));
